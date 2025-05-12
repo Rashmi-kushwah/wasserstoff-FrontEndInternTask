6e4035351a8d6f7b1387a7801549fe5cf6490b85
Rashmi Kushwah <rashmiinfo6@gmail.com>
2025-05-12 13:06:21 +0530

    Initial commit - React client and Node server

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css"; // Import Quill's snow theme
+import "react-quill/dist/quill.snow.css"; 
 import "./quill-custom.css"; // Optional: Tailwind styling override
 import  "../components/quill-custom.css"
 
